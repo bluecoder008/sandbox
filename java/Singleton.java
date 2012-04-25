cc6d4cd1eccfc6e31959ac0ca83b77490a62a9ad
Weilan Wu <weilan.wu@gmail.com>
2012-04-24 21:24:34 -0700

    Cleaned up

--- java/Singleton.java
+++ java/Singleton.java
@@ -13,12 +13,14 @@ public class Singleton {
 
         // Private constructor prevents instantiation from other classes
         private Singleton() { 
-            System.out.println( "Singleton() ctor =" + new Date().toString() );
-            System.out.println( "ts2=" + new Date().toString() );
         }
  
         private static class SingletonHolder {  
-                public static final Singleton instance = new Singleton();
+
+            static {
+                  System.out.println( "ts2=" + new Date().toString() );
+            }
+            public static final Singleton instance = new Singleton();
         }
  
         public static Singleton getInstance() {
