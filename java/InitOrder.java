d4d56a86319d57eaa57ea0817b89bccefe5c7aa9
bluecoder008 <weilan.wu@gmail.com>
2014-03-26 22:55:04 -0700

    Added example for Java initialization order

---
+++ java/InitOrder.java
@@ -0,0 +1,25 @@
+
+public class InitOrder {
+
+    public static InitOrder m = new InitOrder();
+    public InitOrder(){
+        System.out.printf("MAIN CONSTRUCTOR\n");
+    }
+
+    {
+        System.out.printf("NON-STATIC BLOCK\n");
+    }
+
+    static{
+        System.out.printf("STATIC BLOCK\n");
+    }
+
+    //public static InitOrder m = new InitOrder();
+
+
+    public static void main(String... args) {
+        //InitOrder m = new InitOrder();
+        System.out.printf("MAIN METHOD\n");
+
+    }
+}
